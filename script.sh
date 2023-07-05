95d84009767512a6293450800b266125adac6f35
Sourav Das (Dipta) <dip9623@outlook.com>
2023-07-05 22:28:51 +0600

    chore

--- script.sh
+++ script.sh
@@ -13,6 +13,15 @@ echo "$selected_text" > from-i3.txt
 echo "$selected_text" | \
   ./piper --model en-us-amy-low.onnx --output_file ~/Desktop/welcome.wav
 
+# echo "$selected_text" | \
+#   ./piper --model en-us-ryan-high.onnx --output_file ~/Desktop/welcome.wav
+
+# echo "$selected_text" | \
+#   ./piper --model en-us-ryan-medium.onnx --output_file ~/Desktop/welcome.wav
+
+# using ryan the following does not work!! :v
+# Another important thing to note is our use of the extended option when calling bodyParser.urlencoded.
+
 #ffplay -hide_banner -loglevel panic -nostats -autoexit -nodisp  -af "atempo=1.4" ~/Desktop/welcome.wav
 
 # -nodisp = no display
