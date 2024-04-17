8dbd64188111f9e73c49ac54d1e0c03ca242dcdb
Mantresh <ray779@tuta.io>
2024-04-17 11:51:16 +0530

    handle errors in the same way

--- speechd_backend.py
+++ speechd_backend.py
@@ -14,10 +14,14 @@ except ImportError:
 class Speechd(TTS):
     def __init__(self, parsed):
         super().__init__()
+
         if not has_speechd:
-            raise Exception(
+            logger.critical(
                 "The speechd python module is unavailable. Please check if you have speech-dispatcher installed and/or you've enabled --system-site-packages for the virtualenv"
             )
+            self.inited = False
+            return
+
         self.parsed = parsed
         self.sdclient = speechd.SSIPClient(f"tts-reader_{__name__}_{time.time()}")
         self.inited = True
