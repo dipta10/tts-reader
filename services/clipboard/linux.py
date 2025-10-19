b1dbfd5a760c231d13a5f24de8e3cf2ff95d9837
Sourav Das (Dipta) <dip9623@outlook.com>
2025-10-19 20:42:11 +0600

    chore: try both ws-paste and xclip to get selected text

--- services/clipboard/linux.py
+++ services/clipboard/linux.py
@@ -10,17 +10,25 @@ from .ports import Clipboard
 class LinuxClipboard(Clipboard):
     def __init__(self, wayland: bool):
         self._wayland = wayland
-        if self._wayland:
-            self._wl_paste = shutil.which("wl-paste")
-            if not self._wl_paste:
-                raise RuntimeError("Couldn't find the wl-paste binary")
-        else:
-            self._xclip = shutil.which("xclip")
-            if not self._xclip:
-                raise RuntimeError("Couldn't find the xclip binary")
+        self._wl_paste = shutil.which("wl-paste")
+        if self._wayland and not self._wl_paste:
+            raise RuntimeError("Couldn't find the wl-paste binary")
+
+        self._xclip = shutil.which("xclip")
+        if not self._wayland and not self._xclip:
+            raise RuntimeError("Couldn't find the xclip binary")
+
+        self.commands = [
+            [self._wl_paste, "-p"],
+            [self._xclip, "-o", "-selection", "primary"]
+        ]
 
     def read_text(self) -> Optional[str]:
-        cmd = [self._wl_paste, "-p"] if self._wayland else [self._xclip, "-o", "-selection", "primary"]
-        out = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout
-        return out.decode("utf-8")
+        for cmd in self.commands:
+            try:
+                out = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout
+                return out.decode("utf-8")
+            except subprocess.CalledProcessError:
+                pass
 
+        raise RuntimeError("Failed to read clipboard with both wl-paste and xclip")
